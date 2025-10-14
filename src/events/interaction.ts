e7a650e9d667aa47869ea5c49aad25f169ab7ece
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 17:21:15 -0500

    Add interaction event handler for command execution

---
+++ src/events/interaction.ts
@@ -0,0 +1,14 @@
+import { CommandInteraction, Events } from "discord.js";
+import Event from "../classes/Event";
+import getFilesFromDirectory from "../utils/getFilesFromDirectory";
+import { join } from "path";
+import Command from "../classes/Command";
+
+const commands: Command[] = [];
+getFilesFromDirectory(join(__dirname, "../commands"), ".js", commands);
+
+export default new Event(Events.InteractionCreate, false, async (interaction: CommandInteraction) => {
+    if (interaction.isChatInputCommand() === false || interaction.isButton() === true) return;
+
+    commands.find((command) => command.slash.name === interaction.commandName)?.execute(interaction);
+});

cf2df1cf54c0b2fc52dd7b38f7f9ad8f8493753d
Landon Redmond <landon.redmond0@gmail.com>
2025-10-17 20:43:19 -0500

    Refactor source command

--- src/commands/source.ts
+++ src/commands/source.ts
@@ -1,14 +1,5 @@
 import Command, { CommandCategory } from "../classes/Command";
 
-export default new Command(
-    "source",
-    5,
-    CommandCategory.words,
-    {
-        name: "source",
-        description: "The source code of Verbose",
-    },
-    async (interaction) => {
-        interaction.reply("**Verbose** is open source! You can find the code on [GitHub](https://github.com/lanred-dev/verbose)");
-    }
-);
+export default new Command("source", "Get the source code for Verbose", CommandCategory.help, [], async (interaction) => {
+    interaction.reply("**Verbose** is open source! You can find the code on [GitHub](https://github.com/lanred-dev/verbose)");
+});

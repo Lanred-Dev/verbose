3da6a10d4b91ca5a4b8d67189de59a6ed13cdcff
Landon Redmond <landon.redmond0@gmail.com>
2025-10-17 20:43:11 -0500

    Refactor message event handler to increment word counts and track user words

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,6 +1,9 @@
 import { Events, Message } from "discord.js";
 import Event from "../classes/Event";
 import parser from "../utils/parser";
+import { incrementUserWordCount, incrementWordCount } from "../db";
+
+const TRACK_USER_WORDS: boolean = process.env.TRACK_USER_WORDS === "true";
 
 export default new Event(Events.MessageCreate, false, async (message: Message) => {
     if (message.author.bot) return;
@@ -9,5 +12,9 @@ export default new Event(Events.MessageCreate, false, async (message: Message) =
 
     if (words.length === 0) return;
 
-    console.log(`Message from ${message.author.tag}:`, words);
+    words.forEach((word) => {
+        incrementWordCount(word);
+
+        if (TRACK_USER_WORDS) incrementUserWordCount(message.author.id, word);
+    });
 });
